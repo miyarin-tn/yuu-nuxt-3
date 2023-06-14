0c705fef69b1ab385ebd64cfb97a6d0a7828a0e5
Thinh Nguyen <thinh.nguyen@dirox.net>
2023-06-14 23:03:09 +0700

    initialize app

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // DevTools: https://nuxt.com/docs/api/configuration/nuxt-config#devtools
+  devtools: { enabled: true },
+  // Global css: https://nuxt.com/docs/api/configuration/nuxt-config#css
+  css: [
+    '@/assets/css/styles.css',
+  ],
+})
