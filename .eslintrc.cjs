feb675596d686f1bd0163961850d36e321687fb4
Thinh Nguyen <thinh.nguyen@dirox.net>
2023-06-15 14:27:02 +0700

    feat(format): check code format

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+  ],
+  plugins: [],
+  rules: {
+    'no-console': 'off',
+    'vue/html-self-closing': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+}
